3f4e5ca8143f0a5a583ffb6c0a4f2d435d0300c9
Dayne Game <daynegame024@gmail.com>
2017-10-24 10:37:59 +1300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+namespace assignment_3
+{
+    class Program
+    {
+        static List<string> Falconers = new List<string> { "Debbie", "Heidi", "Noel", "Melissa", "Kirsten", "Koshy" };
+        static List<string> Falcons = new List<string> { "Hisan", "Ka Kite", "Siren", "Koa", "Stella", "Mojo" };
+
+
+        static void Main(string[] args)
+        {
+            //Start the program with Clear();
+            Console.Clear();
+
+            Console.WriteLine(ShowFalcons());
+            Console.WriteLine(ShowFalcons());
+            Console.WriteLine(ShowFalcons());
+            Console.WriteLine(ShowFalcons());
+
+            //End the program with blank line and instructions
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+        }
+
+        static int RandomNumber(List<string> myList)
+        {
+
+            var randomNum = new Random();
+            return randomNum.Next(1, myList.Count);
+        }
+
+        static string ShowFalcons()
+        {
+            var falconersSelect = Falconers[RandomNumber(Falconers)];
+            var falconSelect = Falcons[RandomNumber(Falcons)];
+            return $"Falconer: {falconersSelect}\nWith Falcon: {falconSelect}";
+        }
+    }
+}
